d343d570754333435c43d3264ca9ba4c53cd900f
tbutterwith <tbutterwith@gmail.com>
2014-02-25 14:42:50 +0000

    update files

--- AgileProject/src/com/StockTake/SummaryActivity.java
+++ AgileProject/src/com/StockTake/SummaryActivity.java
@@ -14,87 +14,87 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-public class SummaryActivity extends Activity {
+public class SummaryActivity extends Activity
+{
 
 	/** Called when the activity is first created. */
 
 	StockManager myStockmanager;
-
+	
 	/* Create Error Messages */
-	TableLayout table;
+	TableLayout table; 
 	TableRow errorRow;
 	TextView error1;
 	TableRow.LayoutParams params;
-
+	
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		// Get the StockManager
-		myStockmanager = ((StockManager) getApplicationContext());
-
+		myStockmanager = ((StockManager)getApplicationContext());
+		
 		setContentView(R.layout.summary);
-
-		update();
+		
+	    update();
 
 	}
 
 	public void update() {
-
-		table = (TableLayout) this.findViewById(R.id.tableLayout1);
-
+		
+		
+		table = (TableLayout) this.findViewById(R.id.tableLayout1); 
+		
 		errorRow = new TableRow(this);
 		error1 = new TextView(this);
-		params = new TableRow.LayoutParams();
-		params.span = 4;
-
+		params = new TableRow.LayoutParams();  
+	    params.span = 4;
+		
 		if (checkInternetConnection()) {
 			try {
-
+				
 				onClick();
 				myStockmanager.summaryTable(this);
-
-			} catch (Exception e) {
-				/* Parse Error */
-				error1.setText(Html
-						.fromHtml(" <big>Oops!</big><br/><br/> Something went wrong when we tried to retrieve your share portfolio.<br/><br/> Please try again later."));
-				errorRow.addView(error1, params);
-				table.addView(errorRow);
+				
+			} catch(Exception e) {
+				/* Parse Error */ 
+       		error1.setText(Html.fromHtml(" <big>Oops!</big><br/><br/> Something went wrong when we tried to retrieve your share portfolio.<br/><br/> Please try again later."));
+       		errorRow.addView(error1, params);
+                table.addView(errorRow); 
 			}
-
+				
 		} else {
 			/* No Internet Connection */
-			error1.setText(Html
-					.fromHtml(" <big>Oops!</big><br/><br/> It seems there is a problem with your internet connection."));
+			error1.setText(Html.fromHtml(" <big>Oops!</big><br/><br/> It seems there is a problem with your internet connection."));
 			errorRow.addView(error1, params);
-			table.addView(errorRow);
+            table.addView(errorRow);
 		}
 	}
 
 	/* Click Refresh */
 	public void onClick() throws IOException, JSONException {
-
+	
 		myStockmanager.clearPortfolio();
-		myStockmanager.addPortfolioEntry("SN", "Smith & Nephew Plc Ord.", 1219);
-		myStockmanager.addPortfolioEntry("BP", "BP Amoco Plc", 192);
-		myStockmanager.addPortfolioEntry("HSBA", "HSBC Holdings Plc Ord.", 343);
+		myStockmanager.addPortfolioEntry("SN", "S&M", 1219);
+		myStockmanager.addPortfolioEntry("BP", "BP", 192);
+		myStockmanager.addPortfolioEntry("HSBA", "HSBC", 343);
 		myStockmanager.addPortfolioEntry("EXPN", "Experian", 258);
-		myStockmanager.addPortfolioEntry("MKS", "Marks & Spencer Ord.", 485);
-
+		myStockmanager.addPortfolioEntry("MKS", "M&S", 485);
+		
 	}
-
+	
 	private boolean checkInternetConnection() {
-
+	
 		ConnectivityManager conMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-
+	
 		// ARE WE CONNECTED TO THE INTERNET
-		if (conMgr.getActiveNetworkInfo() != null
-				&& conMgr.getActiveNetworkInfo().isAvailable()
-				&& conMgr.getActiveNetworkInfo().isConnected()) {
+		if (conMgr.getActiveNetworkInfo() != null && conMgr.getActiveNetworkInfo().isAvailable() && conMgr.getActiveNetworkInfo().isConnected()) {
 			return true;
 		} else {
 			return false;
 		}
-
+	
 	}
+
 
 }
