3e853c8cb7863a5ade66dcbe0e6b474b8f9173ab
tbutterwith <tbutterwith@gmail.com>
2014-02-25 14:58:52 +0000

    fixed conflicts

--- AgileProject/src/com/StockTake/GraphsActivity.java
+++ AgileProject/src/com/StockTake/GraphsActivity.java
@@ -15,86 +15,86 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 public class GraphsActivity extends Activity {
-	
-	StockManager myStockmanager;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		
-		setContentView(R.layout.graph);
-		
-		super.onCreate(savedInstanceState);
-		
-		// Get the StockManager
-		myStockmanager = ((StockManager)getApplicationContext());
-		
-		setContentView(R.layout.rocket);
-		
-		/* Create Error Messages */
-		TableLayout table = (TableLayout) this.findViewById(R.id.tableLayout3); 
-		TableRow errorRow = new TableRow(this);
-		TextView error1 = new TextView(this);
-		TableRow.LayoutParams params = new TableRow.LayoutParams();  
-	    params.span = 4;
+StockManager myStockmanager;
 
-		/* Init refresh button */
-		//Button button = (Button) findViewById(R.id.btnRefresh);
-		//button.setOnClickListener(this);
+@Override
+public void onCreate(Bundle savedInstanceState)
+{
+super.onCreate(savedInstanceState);
 
-		if (checkInternetConnection()) {
-			try {
-				
-				
-				int rocketplummet = myStockmanager.rocketTable(this);
-				if (rocketplummet == 0) { 
-					
-					error1.setText(Html.fromHtml("No graphs yet!"));
-	        		errorRow.addView(error1, params);
-	                table.addView(errorRow);
-				}
-				
-			} catch(Exception e) {
-				/* Parse Error */ 
-        		error1.setText(Html.fromHtml(" <big>Oops!</big><br/><br/> Something went wrong when we tried to retrieve your share portfolio.<br/><br/> Please try again later."));
-        		errorRow.addView(error1, params);
-                table.addView(errorRow);
-			}
-				
-		} else {
-			/* No Internet Connection */
-			error1.setText(Html.fromHtml(" <big>Oops!</big><br/><br/> It seems there is a problem with your internet connection."));
-			errorRow.addView(error1, params);
-            table.addView(errorRow);
-		}
+setContentView(R.layout.graph);
 
-	}
-	
-	private boolean checkInternetConnection() {
-		
-		ConnectivityManager conMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-	
-		// ARE WE CONNECTED TO THE INTERNET
-		if (conMgr.getActiveNetworkInfo() != null && conMgr.getActiveNetworkInfo().isAvailable() && conMgr.getActiveNetworkInfo().isConnected()) {
-			return true;
-		} else {
-			return false;
-		}
-	
-	}
-	
-	public void getData()
-	{
-		String stockname = "SN";
-		String [] historic;
-		FeedParser HistoricData = new FeedParser();
-		historic = HistoricData.getHistoricFeed(stockname);
-		
-		for(int i = 0; i < historic.length; i++) {
-			Log.v("name", "4 : " + historic[i]);
-		}
-	}
+super.onCreate(savedInstanceState);
 
-	
+// Get the StockManager
+myStockmanager = ((StockManager)getApplicationContext());
+
+setContentView(R.layout.rocket);
+
+/* Create Error Messages */
+TableLayout table = (TableLayout) this.findViewById(R.id.tableLayout3);
+TableRow errorRow = new TableRow(this);
+TextView error1 = new TextView(this);
+TableRow.LayoutParams params = new TableRow.LayoutParams();
+params.span = 4;
+
+/* Init refresh button */
+//Button button = (Button) findViewById(R.id.btnRefresh);
+//button.setOnClickListener(this);
+
+if (checkInternetConnection()) {
+try {
+
+
+int rocketplummet = myStockmanager.rocketTable(this);
+if (rocketplummet == 0) {
+
+error1.setText(Html.fromHtml("No graphs yet!"));
+errorRow.addView(error1, params);
+table.addView(errorRow);
+}
+
+} catch(Exception e) {
+/* Parse Error */
+error1.setText(Html.fromHtml(" <big>Oops!</big><br/><br/> Something went wrong when we tried to retrieve your share portfolio.<br/><br/> Please try again later."));
+errorRow.addView(error1, params);
+table.addView(errorRow);
+}
+
+} else {
+/* No Internet Connection */
+error1.setText(Html.fromHtml(" <big>Oops!</big><br/><br/> It seems there is a problem with your internet connection."));
+errorRow.addView(error1, params);
+table.addView(errorRow);
+}
+
+}
+
+private boolean checkInternetConnection() {
+
+ConnectivityManager conMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+// ARE WE CONNECTED TO THE INTERNET
+if (conMgr.getActiveNetworkInfo() != null && conMgr.getActiveNetworkInfo().isAvailable() && conMgr.getActiveNetworkInfo().isConnected()) {
+return true;
+} else {
+return false;
+}
+
+}
+
+public void getData()
+{
+String stockname = "SN";
+String [] historic;
+FeedParser HistoricData = new FeedParser();
+historic = HistoricData.getHistoricFeed(stockname);
+
+for(int i = 0; i < historic.length; i++) {
+Log.v("name", "4 : " + historic[i]);
+}
+}
+
+
 }
