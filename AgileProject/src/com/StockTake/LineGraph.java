f905ff2a52905d7579be7033068d02f36fea4622
anzbrown <a.n.z.brown@dundee.ac.uk>
2014-02-25 14:21:57 +0000

    Line Graph
    updated line graph.java

--- AgileProject/src/com/StockTake/LineGraph.java
+++ AgileProject/src/com/StockTake/LineGraph.java
@@ -8,15 +8,14 @@ import com.androidplot.xy.*;
 import java.util.Arrays;
 
 /**
- * A straightforward example of using AndroidPlot to plot some data.
+ * Using AndroidPlot to plot some data.
  */
 public class LineGraph extends Activity
 {
 
     private XYPlot plot;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState)
+    public void onCreate(Bundle savedInstanceState, Number[] stockValues)
     {
         super.onCreate(savedInstanceState);
 
@@ -24,25 +23,16 @@ public class LineGraph extends Activity
         // on ICS+ devices :-)
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
                                  WindowManager.LayoutParams.FLAG_SECURE);
-        setContentView(R.layout.simple_xy_plot_example);
+        setContentView(R.layout.graph);
 
         // initialize our XYPlot reference:
-        plot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
-
-        // Create a couple arrays of y-values to plot:
-        Number[] series1Numbers = {1, 8, 5, 2, 7, 4};
-        Number[] series2Numbers = {4, 6, 3, 8, 2, 10};
+        plot = (XYPlot) findViewById(R.id.graph);
         
-        Number[] stockData;
-
         // Turn the above arrays into XYSeries':
         XYSeries series1 = new SimpleXYSeries(
-                Arrays.asList(stockData),          // SimpleXYSeries takes a List so turn our array into a List
+                Arrays.asList(stockValues),          // SimpleXYSeries takes a List so turn our array into a List
                 SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
                 "Series1");                             // Set the display title of the series
-
-        // same as above
-        XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
 
         // Create a formatter to use for drawing a series using LineAndPointRenderer
         // and configure it from xml:
@@ -54,12 +44,6 @@ public class LineGraph extends Activity
         // add a new series' to the xyplot:
         plot.addSeries(series1, series1Format);
 
-        // same as above:
-        LineAndPointFormatter series2Format = new LineAndPointFormatter();
-        series2Format.setPointLabelFormatter(new PointLabelFormatter());
-        series2Format.configure(getApplicationContext(),
-                R.xml.line_point_formatter_with_plf2);
-        plot.addSeries(series2, series2Format);
 
         // reduce the number of range labels
         plot.setTicksPerRangeLabel(3);
