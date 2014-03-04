fc7b81801327f61592e3a31773097a37cb5db17d
VChow <victorchow_507@hotmail.com>
2014-03-04 14:12:41 +0000

    Removed warnings from all java files apart from StockManager.java

--- AgileProject/src/com/StockTake/StockManager.java
+++ AgileProject/src/com/StockTake/StockManager.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -12,20 +11,17 @@ import java.util.Map;
 import java.util.NavigableMap;
 import java.util.TreeMap;
 
-
 import android.app.Activity;
 import android.app.Application;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.text.Html;
-import android.util.Log;
 import android.view.Gravity;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-public class StockManager extends Application
-{
+public class StockManager extends Application {
 	// private Finance stockObj;
 	private HashMap<Finance, Float> portfolio = new HashMap<Finance, Float>();
 	private HashMap<String, String> stockNamesLong = new HashMap<String, String>();
@@ -34,38 +30,32 @@ public class StockManager extends Application
 
 	private String myState;
 
-	public String getState()
-	{
+	public String getState() {
 		return myState;
 	}
 
-	public void setState(String s)
-	{
+	public void setState(String s) {
 		myState = s;
 	}
-	
-	public HashMap<Finance, Float> getPortfolio()
-	{
+
+	public HashMap<Finance, Float> getPortfolio() {
 		return portfolio;
 	}
-	
-	public void setPortfolio(HashMap<Finance, Float> portfolio)
-	{
+
+	public void setPortfolio(HashMap<Finance, Float> portfolio) {
 		this.portfolio = portfolio;
 	}
 
-	public void clearPortfolio()
-	{
+	public void clearPortfolio() {
 		portfolio.clear();
 		stockNames.clear();
 		stockNamesLong.clear();
 	}
 
-	public Finance createFinanceObject(String stockCode) throws IOException
-	{
+	public Finance createFinanceObject(String stockCode) throws IOException {
 
 		Finance newStock = new Finance();
-		
+
 		newParse.getFeed(newStock, stockCode);
 		newStock.calcRun();
 		newStock.calcRocketPlummet();
@@ -74,14 +64,12 @@ public class StockManager extends Application
 
 	}
 
-	public boolean addPortfolioEntry(String stockCode, String stockNameLong, int numberOfShares)
-			throws IOException
-	{
+	public boolean addPortfolioEntry(String stockCode, String stockNameLong,
+			int numberOfShares) throws IOException {
 
 		float shareQuantity = (float) numberOfShares;
 		Finance stockObj = createFinanceObject(stockCode);
-		if (portfolio.containsKey(stockObj))
-		{
+		if (portfolio.containsKey(stockObj)) {
 			return false;
 		}
 		stockObj.setName(stockNameLong);
@@ -91,28 +79,25 @@ public class StockManager extends Application
 		return true;
 	}
 
-	public float getPortfolioTotal()
-	{
+	public float getPortfolioTotal() {
 		float value = 0;
-		if (portfolio.isEmpty())
-		{
+		if (portfolio.isEmpty()) {
 			return 0;
 		}
-		for (Finance stockObj : portfolio.keySet())
-		{
+		for (Finance stockObj : portfolio.keySet()) {
 			value += stockObj.getLast() * portfolio.get(stockObj);
 		}
 		return value;
 	}
 
-	public void summaryTable(Activity contextActivity)
-	{
+	public void summaryTable(Activity contextActivity) {
 
-		TableLayout table = (TableLayout) contextActivity.findViewById(R.id.tableLayout1); // Find
-																							// TableLayout
-																							// defined
-																							// in
-																							// main.xml
+		TableLayout table = (TableLayout) contextActivity
+				.findViewById(R.id.tableLayout1); // Find
+													// TableLayout
+													// defined
+													// in
+													// main.xml
 
 		table.setStretchAllColumns(true);
 		table.setShrinkAllColumns(true);
@@ -129,20 +114,17 @@ public class StockManager extends Application
 
 		TableRow rowTotal = new TableRow(contextActivity);
 		TextView portfolioTotal = new TextView(contextActivity);
-		
+
 		NavigableMap sortedMap = sortPortfolioByValue();
 		// Now sort...
 		Iterator<?> it = sortedMap.entrySet().iterator();
-		while(it.hasNext())
-		{
-			Map.Entry pairs = (Map.Entry)it.next();
+		while (it.hasNext()) {
+			Map.Entry pairs = (Map.Entry) it.next();
 			String currStockName = pairs.getValue().toString();
-			
+
 			Finance stockObj = null;
-			for (Finance thisObj : portfolio.keySet())
-			{
-				if (thisObj.getName().equals(currStockName))
-				{
+			for (Finance thisObj : portfolio.keySet()) {
+				if (thisObj.getName().equals(currStockName)) {
 					stockObj = thisObj;
 					break; // fail fast
 				}
@@ -157,41 +139,52 @@ public class StockManager extends Application
 			float thisStockValue = stockObj.getLast();
 
 			// half up rounding mode - so reduces errors to +/- £1
-			BigDecimal stockValueRounded = new BigDecimal(Double.toString(thisStockValue));
-			stockValueRounded = stockValueRounded.setScale(0, BigDecimal.ROUND_HALF_UP);
+			BigDecimal stockValueRounded = new BigDecimal(
+					Double.toString(thisStockValue));
+			stockValueRounded = stockValueRounded.setScale(0,
+					BigDecimal.ROUND_HALF_UP);
 			float subTotal = portfolio.get(stockObj) * thisStockValue;
 
-			//String longName = stockNamesLong.get(stockObj.getName().toString());
+			// String longName =
+			// stockNamesLong.get(stockObj.getName().toString());
 			String longName = currStockName;
-			
+
 			stockName[stockCounter].setText(longName);
-			stockName[stockCounter].setTypeface(Typeface.DEFAULT, Typeface.BOLD);
-			//stockName[stockCounter].setTextColor(Color.rgb(58, 128, 255));
+			stockName[stockCounter]
+					.setTypeface(Typeface.DEFAULT, Typeface.BOLD);
+			// stockName[stockCounter].setTextColor(Color.rgb(58, 128, 255));
 			stockName[stockCounter].setTextSize(24f);
 			stockName[stockCounter].setHeight(130);
 			stockName[stockCounter].setWidth(500);
-			stockName[stockCounter].setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL);
+			stockName[stockCounter].setGravity(Gravity.LEFT
+					| Gravity.CENTER_VERTICAL);
 
 			/*
-			stockShares[stockCounter].setText(String.format("%,3.0f", portfolio.get(stockObj)));
-			stockShares[stockCounter].setGravity(Gravity.RIGHT | Gravity.CENTER_VERTICAL);
-			stockShares[stockCounter].setTextSize(20f);
-			stockShares[stockCounter].setSingleLine(true);
+			 * stockShares[stockCounter].setText(String.format("%,3.0f",
+			 * portfolio.get(stockObj)));
+			 * stockShares[stockCounter].setGravity(Gravity.RIGHT |
+			 * Gravity.CENTER_VERTICAL);
+			 * stockShares[stockCounter].setTextSize(20f);
+			 * stockShares[stockCounter].setSingleLine(true);
+			 * 
+			 * stockValue[stockCounter].setText("£" + String.format("%.2f",
+			 * thisStockValue));
+			 * stockValue[stockCounter].setGravity(Gravity.RIGHT |
+			 * Gravity.CENTER_VERTICAL);
+			 * stockValue[stockCounter].setTextSize(20f);
+			 * stockValue[stockCounter].setSingleLine(true);
+			 */
 
-			stockValue[stockCounter].setText("£" + String.format("%.2f", thisStockValue));
-			stockValue[stockCounter].setGravity(Gravity.RIGHT | Gravity.CENTER_VERTICAL);
-			stockValue[stockCounter].setTextSize(20f);
-			stockValue[stockCounter].setSingleLine(true);
-			*/
-
-			stockTotal[stockCounter].setText("£  " + String.format("%,3.0f", subTotal));
-			stockTotal[stockCounter].setGravity(Gravity.RIGHT | Gravity.CENTER_VERTICAL);
+			stockTotal[stockCounter].setText("£  "
+					+ String.format("%,3.0f", subTotal));
+			stockTotal[stockCounter].setGravity(Gravity.RIGHT
+					| Gravity.CENTER_VERTICAL);
 			stockTotal[stockCounter].setTextSize(24f);
 			stockTotal[stockCounter].setSingleLine(true);
 
 			rowStock[stockCounter].addView(stockName[stockCounter]);
-			//rowStock[stockCounter].addView(stockShares[stockCounter]);
-			//rowStock[stockCounter].addView(stockValue[stockCounter]);
+			// rowStock[stockCounter].addView(stockShares[stockCounter]);
+			// rowStock[stockCounter].addView(stockValue[stockCounter]);
 			rowStock[stockCounter].addView(stockTotal[stockCounter]);
 
 			table.addView(rowStock[stockCounter]);
@@ -200,7 +193,8 @@ public class StockManager extends Application
 
 		}
 
-		String totalVal = "Total Value:     £  " + String.format("%,.0f", getPortfolioTotal());
+		String totalVal = "Total Value:     £  "
+				+ String.format("%,.0f", getPortfolioTotal());
 		portfolioTotal.setText(totalVal);
 		portfolioTotal.setTextSize(28f);
 		portfolioTotal.setHeight(100);
@@ -214,39 +208,35 @@ public class StockManager extends Application
 		table.addView(rowTotal);
 
 	}
-	
-	public NavigableMap sortPortfolioByValue()
-	{
+
+	public NavigableMap sortPortfolioByValue() {
 		TreeMap<Float, String> portfolioValue = new TreeMap<Float, String>();
 		Finance currentStock = new Finance();
 		float stocksHeld;
 		float total;
 
 		Iterator it = portfolio.entrySet().iterator();
-		while(it.hasNext())
-		{
-			Map.Entry pairs = (Map.Entry)it.next();
-			currentStock = (Finance)pairs.getKey();
-			stocksHeld = (Float)pairs.getValue();
+		while (it.hasNext()) {
+			Map.Entry pairs = (Map.Entry) it.next();
+			currentStock = (Finance) pairs.getKey();
+			stocksHeld = (Float) pairs.getValue();
 			total = stocksHeld * currentStock.getLast();
-			
+
 			portfolioValue.put(total, currentStock.getName());
 		}
 		NavigableMap mapMap = portfolioValue.descendingMap();
-		
+
 		return mapMap;
 	}
-	
-	
 
-	public int volumeTable(Activity contextActivity)
-	{
+	public int volumeTable(Activity contextActivity) {
 
-		TableLayout table = (TableLayout) contextActivity.findViewById(R.id.tableLayout2); // Find
-																							// TableLayout
-																							// defined
-																							// in
-																							// main.xml
+		TableLayout table = (TableLayout) contextActivity
+				.findViewById(R.id.tableLayout2); // Find
+													// TableLayout
+													// defined
+													// in
+													// main.xml
 
 		table.setStretchAllColumns(true);
 		table.setShrinkAllColumns(true);
@@ -265,10 +255,8 @@ public class StockManager extends Application
 		{
 
 			Finance stockObj = null;
-			for (Finance thisObj : portfolio.keySet())
-			{
-				if (thisObj.getName().equals(currStockName))
-				{
+			for (Finance thisObj : portfolio.keySet()) {
+				if (thisObj.getName().equals(currStockName)) {
 					stockObj = thisObj;
 					break; // fail fast
 				}
@@ -277,47 +265,48 @@ public class StockManager extends Application
 			rowRun[stockCounter] = new TableRow(contextActivity);
 			runStock[stockCounter] = new TextView(contextActivity);
 			runLabel[stockCounter] = new TextView(contextActivity);
-			runStock[stockCounter].setText(stockNamesLong.get(stockObj.getName().toString()));
+			runStock[stockCounter].setText(stockNamesLong.get(stockObj
+					.getName().toString()));
 			runStock[stockCounter].setTextSize(26f);
 			runStock[stockCounter].setHeight(130);
 			runStock[stockCounter].setGravity(Gravity.CENTER_VERTICAL);
 			runLabel[stockCounter].setTypeface(Typeface.DEFAULT, Typeface.BOLD);
 			runLabel[stockCounter].setTextSize(26f);
 			runLabel[stockCounter].setHeight(130);
-			runLabel[stockCounter].setGravity(Gravity.CENTER_VERTICAL | Gravity.RIGHT);
+			runLabel[stockCounter].setGravity(Gravity.CENTER_VERTICAL
+					| Gravity.RIGHT);
 
-			if (stockObj.isRun() && !stockObj.isPlummet() && !stockObj.isRocket())
-			{
-				
-				runLabel[stockCounter].setText(Html.fromHtml("<font color=\"#CCCC33\">Run</font>"));
+			if (stockObj.isRun() && !stockObj.isPlummet()
+					&& !stockObj.isRocket()) {
+
+				runLabel[stockCounter].setText(Html
+						.fromHtml("<font color=\"#CCCC33\">Run</font>"));
 				rowRun[stockCounter].addView(runStock[stockCounter]);
 				rowRun[stockCounter].addView(runLabel[stockCounter]);
 				runs++;
-			}
-			else if(stockObj.isRun() && stockObj.isPlummet())
-			{
-				runLabel[stockCounter].setText(Html.fromHtml("<font color=\"#CCCC33\">Run</font> & <font color='red'>Plummet</font>"));
+			} else if (stockObj.isRun() && stockObj.isPlummet()) {
+				runLabel[stockCounter]
+						.setText(Html
+								.fromHtml("<font color=\"#CCCC33\">Run</font> & <font color='red'>Plummet</font>"));
 				rowRun[stockCounter].addView(runStock[stockCounter]);
 				rowRun[stockCounter].addView(runLabel[stockCounter]);
 				runs++;
-			}
-			else if(stockObj.isRun() && stockObj.isRocket())
-			{
-				runLabel[stockCounter].setText(Html.fromHtml("<font color=\"#CCCC33\">Run</font> & <font color='green'>Rocket</font>"));
+			} else if (stockObj.isRun() && stockObj.isRocket()) {
+				runLabel[stockCounter]
+						.setText(Html
+								.fromHtml("<font color=\"#CCCC33\">Run</font> & <font color='green'>Rocket</font>"));
 				rowRun[stockCounter].addView(runStock[stockCounter]);
 				rowRun[stockCounter].addView(runLabel[stockCounter]);
 				runs++;
-			}
-			else if(!stockObj.isRun() && stockObj.isPlummet())
-			{
-				runLabel[stockCounter].setText(Html.fromHtml("<font color='red'>Plummet</font>"));
+			} else if (!stockObj.isRun() && stockObj.isPlummet()) {
+				runLabel[stockCounter].setText(Html
+						.fromHtml("<font color='red'>Plummet</font>"));
 				rowRun[stockCounter].addView(runStock[stockCounter]);
 				rowRun[stockCounter].addView(runLabel[stockCounter]);
 				runs++;
-			}
-			else if(!stockObj.isRun() && stockObj.isRocket())
-			{
-				runLabel[stockCounter].setText(Html.fromHtml("<font color='green'>Rocket</font>"));
+			} else if (!stockObj.isRun() && stockObj.isRocket()) {
+				runLabel[stockCounter].setText(Html
+						.fromHtml("<font color='green'>Rocket</font>"));
 				rowRun[stockCounter].addView(runStock[stockCounter]);
 				rowRun[stockCounter].addView(runLabel[stockCounter]);
 				runs++;
@@ -333,14 +322,14 @@ public class StockManager extends Application
 
 	}
 
-	public int rocketTable(Activity contextActivity)
-	{
+	public int rocketTable(Activity contextActivity) {
 
-		TableLayout table = (TableLayout) contextActivity.findViewById(R.id.tableLayout3); // Find
-																							// TableLayout
-																							// defined
-																							// in
-																							// main.xml
+		TableLayout table = (TableLayout) contextActivity
+				.findViewById(R.id.tableLayout3); // Find
+													// TableLayout
+													// defined
+													// in
+													// main.xml
 
 		table.setStretchAllColumns(true);
 		table.setShrinkAllColumns(true);
@@ -359,10 +348,8 @@ public class StockManager extends Application
 		{
 
 			Finance stockObj = null;
-			for (Finance thisObj : portfolio.keySet())
-			{
-				if (thisObj.getName().equals(currStockName))
-				{
+			for (Finance thisObj : portfolio.keySet()) {
+				if (thisObj.getName().equals(currStockName)) {
 					stockObj = thisObj;
 					break; // fail fast
 				}
@@ -377,32 +364,28 @@ public class StockManager extends Application
 			rocketStock[stockCounter].setTextSize(20f);
 			rocketStock[stockCounter].setHeight(100);
 			rocketStock[stockCounter].setGravity(Gravity.CENTER_VERTICAL);
-			rocketStock[stockCounter].setText(stockNamesLong.get(stockObj.getName().toString()));
+			rocketStock[stockCounter].setText(stockNamesLong.get(stockObj
+					.getName().toString()));
 
-			if (stockObj.isRocket())
-			{
-				rocketState[stockCounter].setText(Html.fromHtml("<font color='green'>Rocket</font>"));
+			if (stockObj.isRocket()) {
+				rocketState[stockCounter].setText(Html
+						.fromHtml("<font color='green'>Rocket</font>"));
+				rowRocket[stockCounter].addView(rocketStock[stockCounter]);
+				rowRocket[stockCounter].addView(rocketState[stockCounter]);
+				rocketplummet++;
+			} else if (stockObj.isPlummet()) {
+				rocketState[stockCounter].setText(Html
+						.fromHtml("<font color='red'>Plummet</font>"));
 				rowRocket[stockCounter].addView(rocketStock[stockCounter]);
 				rowRocket[stockCounter].addView(rocketState[stockCounter]);
 				rocketplummet++;
 			}
-			else
-				if (stockObj.isPlummet())
-				{
-					rocketState[stockCounter].setText(Html.fromHtml("<font color='red'>Plummet</font>"));
-					rowRocket[stockCounter].addView(rocketStock[stockCounter]);
-					rowRocket[stockCounter].addView(rocketState[stockCounter]);
-					rocketplummet++;
-				}
 
 			table.addView(rowRocket[stockCounter]);
 
 			stockCounter++;
 
 		}
-
 		return rocketplummet;
-
 	}
-
 }
