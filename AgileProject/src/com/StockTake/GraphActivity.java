2019fa3b52577b087d6b9b5bb6d6218b500265e6
anzbrown <a.n.z.brown@dundee.ac.uk>
2014-03-05 10:47:17 +0000

    fixed time spinner

--- AgileProject/src/com/StockTake/GraphActivity.java
+++ AgileProject/src/com/StockTake/GraphActivity.java
@@ -71,7 +71,7 @@ public class GraphActivity extends Activity {
 		ArrayAdapter<String> dataAdapter1 = new ArrayAdapter<String>
 		(this, android.R.layout.simple_spinner_item,timeArray);
 
-		dataAdapter.setDropDownViewResource
+		dataAdapter1.setDropDownViewResource
 		(android.R.layout.simple_spinner_dropdown_item);
 
 		spinner_time.setAdapter(dataAdapter1);
